cb4d0eab1b58cbbb836d67017276ae4f98f14712
Thomas Theunen <thomas.theunen@forward.eu>
2020-03-22 12:54:18 +0100

    Add JSDoc

--- cartridges/int_botblocker/cartridge/scripts/model/ipAddress.js
+++ cartridges/int_botblocker/cartridge/scripts/model/ipAddress.js
@@ -11,14 +11,35 @@ function IpAddress(ip, count) {
     this.count = count;
 }
 
+/**
+ * The first threshold of requests an IP has made within the cache time period.
+ *
+ * Note: When this threshold is reached, we check if the user agent is valid or not.
+ *
+ * @returns {integer} - Wether or not the IP is below the threshold.
+ */
 IpAddress.prototype.isBelowFirstThreshold = function () {
     return this.count < 1200;
 };
 
+/**
+ * The second threshold of requests an IP has made within the cache time period.
+ *
+ * Note: The request is automatically blocked and redirected.
+ *
+ * @returns {integer} - Wether or not the IP is below the threshold.
+ */
 IpAddress.prototype.isBelowSecondThreshold = function () {
     return this.count < 2400;
 };
 
+/**
+ * The third threshold of requests an IP has made within the cache time period.
+ *
+ * Note: The request is automatically blocked and redirected.
+ *
+ * @returns {integer} - Wether or not the IP is below the threshold.
+ */
 IpAddress.prototype.isBelowThirdThreshold = function () {
     return this.count < 4600;
 };
